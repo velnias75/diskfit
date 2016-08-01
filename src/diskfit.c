29ba5702b49d7a71c03b5670d2a59591046f1681
Heiko Schaefer <heiko@rangun.de>
2016-08-01 10:14:54 +0200

    using wordexp for filename expansion

--- src/diskfit.c
+++ src/diskfit.c
@@ -4,11 +4,35 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <math.h>
+#include <wordexp.h>
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 typedef struct {
   char *fname;
   off_t fsize;
 } FITEM;
+
+const char *hrsize(off_t s) {
+  
+  char *r = malloc(1024 * sizeof(char));
+  const double d = log(s)/M_LN2;
+  
+  if(d >= 30.0) {
+    snprintf(r, 1023, "%.2f GByte", (float)s/1073741824.0f);
+  } else if(d >= 20.0) {
+    snprintf(r, 1023, "%.2f MByte", (float)s/1048576.0f);
+  } else if(d >= 10.0) {
+    snprintf(r, 1023, "%.2f KByte", (float)s/1024.0f);
+  } else {
+    snprintf(r, 1023, "%ld Byte", s);
+  }
+  
+  return r;
+}
 
 off_t sum(FITEM *item, int n) {
   
@@ -34,6 +58,7 @@ void swap(FITEM *a, FITEM *b) {
 void permute(FITEM *array, int i, int length, off_t target) { 
   
   int l, k = length - 1;
+  const char *hrs;
   off_t s = 0;
   
   if(length == i) {
@@ -43,7 +68,8 @@ void permute(FITEM *array, int i, int length, off_t target) {
     if(s <= target) {
       fprintf(stdout, "[ ");
       for(l = 0; l < k + 1; ++l) fprintf(stdout, "'%s' ", array[l].fname);
-      fprintf(stdout, "] = %.2f GByte\n", (float)s/1073741824.0f);
+      fprintf(stdout, "] = %s\n", (hrs = hrsize(s)));
+      free((void *)hrs);
     }
     
     return;
@@ -62,12 +88,14 @@ void permute(FITEM *array, int i, int length, off_t target) {
 
 int main(int argc, char *argv[]) {
   
+  size_t j;
   int i, nitems = 0;
   FITEM *fitems = NULL;
-  off_t tsize = 0, tg = atol(argv[1]);
+  off_t tsize = 0, tg = argc > 1 ? atol(argv[1]) : 0L;
+  const char *hr_tot, *hr_tg;
   struct stat st;
   
-  fprintf(stderr, "DiskFit - (c) 2016 by Heiko Schaefer <heiko@rangun.de>\n");
+  fprintf(stderr, PACKAGE_STRING " - (c) 2016 by Heiko Schaefer <heiko@rangun.de>\n");
   
   if(argc < 3) {
     
@@ -76,30 +104,40 @@ int main(int argc, char *argv[]) {
     
   } else {
     
+    wordexp_t p;
+    
+    memset(&p, 0, sizeof(wordexp_t));
+    
     tg = tg == 0 ? 4724464025 : tg;
     
-    fitems = calloc(argc - 2, sizeof(FITEM));
+    for(i = 0; i < argc - 2; ++i) wordexp(argv[i+2], &p, WRDE_NOCMD|WRDE_APPEND);
     
-    for(i = 0; i < argc - 2; ++i) {
+    fitems = calloc(p.we_wordc, sizeof(FITEM));
       
-      if(!stat(argv[i+2], &st)) {
-	
+    for(j = 0; j < p.we_wordc; ++j) {
+      
+      if(!stat(p.we_wordv[j], &st)) {
+      
 	tsize += st.st_size;
-      
-	fitems[nitems].fname = argv[i+2];
-	fitems[nitems].fsize = st.st_size;
+	
+	fitems[j].fname = p.we_wordv[j];
+	fitems[j].fsize = st.st_size;
 	
 	++nitems;
-	
+
       }
     }
     
     permute(fitems, 0, nitems, tg);
     
     free(fitems);
+    wordfree(&p);
     
-    fprintf(stderr, "Total size: %.2f GByte - Target size: %.2f GByte\n", 
-	    ((float)tsize)/1073741824.0f, (float)tg/1073741824.0f);
+    fprintf(stderr, "Total size: %s - Target size: %s\n", 
+	    (hr_tot = hrsize(tsize)), (hr_tg = hrsize(tg)));
+    
+    free((void *)hr_tot);
+    free((void *)hr_tg);
   }
   
   return EXIT_SUCCESS;
